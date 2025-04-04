abacce47123fb36eeafb92d82ec3a84afdbe2688
keemhee <eirmonar@gmail.com>
2025-04-04 13:39:36 +0900

    Updated
    사용자 정의 인터페이스 메시지 연습
    
    ros2 run my_first_pkg sensor_status_pub
    ros2 run my_first_pkg sensor_status_sub
    
    1초마다 10부터 50 사이의 무작위 온도값 생성해 sensor_status 토픽으로 발행
    온도가 30을 초과하면 모터가 준비된 상태로 간주

--- src/my_first_pkg/my_first_pkg/sensor_status_sub.py
+++ src/my_first_pkg/my_first_pkg/sensor_status_sub.py
@@ -1,25 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3  # 스크립트를 실행할 때 사용할 Python 인터프리터를 지정
 
-import rclpy
-from rclpy.node import Node
-from my_interfaces.msg import SensorStatus
+import rclpy  # ROS 2 Python 클라이언트 라이브러리 가져오기
+from rclpy.node import Node  # ROS 2 노드 클래스 가져오기
+from my_interfaces.msg import SensorStatus  # 사용자 정의 메시지 타입 가져오기
 
-class SensorStatusSubNode(Node):
-    def __init__(self):
-        super().__init__("sensor_status_sub")
-        self.subscriber_ = self.create_subscription(SensorStatus, "sensor_status", self.motor_callback, 10)
-        self.get_logger().info("sensor status sub node has been started.")
-
-    def motor_callback(self, msg):
-        self.get_logger().info(f"{msg.debug_messages}")
-
-
-def main(args=None):
-    rclpy.init(args = args)
-    node = SensorStatusSubNode()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
-
-if __name__=="__main__":
-    main()
+class SensorStatusSubNode(Node):  # ROS 2 노드를 상속받는 SensorStatusSubNode 클래스 정의
+   def __init__(self):  # 클래스 초기화 메서드
+       super().__init__("sensor_status_sub")  # 부모 클래스 초기화 및 노드 이름을 "sensor_status_sub"으로 설정
+       self.subscriber_ = self.create_subscription(SensorStatus, "sensor_status", self.motor_callback, 10)  # "sensor_status" 토픽을 구독하고 메시지 수신 시 motor_callback 호출, 큐 크기는 10
+       self.get_logger().info("sensor status sub node has been started.")  # 노드가 시작되었다는 로그 메시지 출력
+       
+   def motor_callback(self, msg):  # 메시지 수신 시 호출되는 콜백 함수
+       self.get_logger().info(f"{msg.debug_messages}")  # 수신한 메시지의 debug_messages 값을 로그로 출력
+       
+def main(args=None):  # 메인 함수 정의
+   rclpy.init(args = args)  # ROS 2 시스템 초기화
+   node = SensorStatusSubNode()  # SensorStatusSubNode 클래스의 인스턴스 생성
+   rclpy.spin(node)  # 노드가 종료될 때까지 콜백 함수 처리를 위해 대기
+   node.destroy_node()  # 노드 종료
+   rclpy.shutdown()  # ROS 2 시스템 종료
+   
+if __name__=="__main__":  # 스크립트가 직접 실행될 때만 아래 코드 실행
+   main()  # 메인 함수 호출
